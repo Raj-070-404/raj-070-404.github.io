87cb1146e64011b6809ea4307b3ff54c9eff7089
Raj-070-404 <emmanuelraj050907@gmail.com>
2026-02-03 18:04:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let map = L.map('map').setView([17.3850, 78.4867], 12);
+let routeLayer;
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
@@ -11,20 +12,43 @@ async function getCoords(place) {
 }
 
 async function planTrip() {
+  if (routeLayer) {
+    map.removeLayer(routeLayer);
+  }
+
   let start = document.getElementById("start").value;
+  let breakfast = document.getElementById("breakfast").value;
+  let scenic = document.getElementById("scenic").value;
+  let lunch = document.getElementById("lunch").value;
   let end = document.getElementById("end").value;
+  let mode = document.getElementById("mode").value;
 
-  let startCoords = await getCoords(start);
-  let endCoords = await getCoords(end);
+  let locations = [];
 
-  let routeURL = `https://router.project-osrm.org/route/v1/driving/${startCoords[1]},${startCoords[0]};${endCoords[1]},${endCoords[0]}?overview=full&geometries=geojson`;
+  if (start) locations.push(start);
+  if (breakfast) locations.push(breakfast);
+  if (scenic) locations.push(scenic);
+  if (lunch) locations.push(lunch);
+  if (end) locations.push(end);
+
+  let coordsList = [];
+
+  for (let place of locations) {
+    let coords = await getCoords(place);
+    coordsList.push(`${coords[1]},${coords[0]}`);
+  }
+
+  let routeURL = `https://router.project-osrm.org/route/v1/${mode}/${coordsList.join(";")}?overview=full&geometries=geojson`;
 
   let routeData = await fetch(routeURL).then(res => res.json());
 
   let route = routeData.routes[0].geometry;
+  let distance = routeData.routes[0].distance;
 
-  L.geoJSON(route).addTo(map);
+  routeLayer = L.geoJSON(route).addTo(map);
+
+  map.fitBounds(routeLayer.getBounds());
 
   document.getElementById("info").innerText =
-    "Distance: " + (routeData.routes[0].distance / 1000).toFixed(2) + " km";
+    `Mode: ${mode.toUpperCase()} | Distance: ${(distance / 1000).toFixed(2)} km`;
 }
